f33d4537466cff07421a832352bb76e9d77a28a1
Efthakhar <efthakharbin@gmail.com>
2023-06-28 19:59:51 +0600

    Add primer css file

--- assets/src/router/index.js
+++ assets/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import OverView from '../views/OverView.vue'
 
 
 const router = createRouter({
@@ -7,8 +7,8 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'overview',
+      component: OverView
     },
 
     {
