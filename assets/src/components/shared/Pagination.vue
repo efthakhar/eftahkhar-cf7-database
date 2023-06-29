c01348464c44f33a77c5ff68c90f41c4846cc167
Efthakhar <efthakharbin@gmail.com>
2023-06-29 14:50:24 +0600

    add new pagination view

--- assets/src/components/shared/Pagination.vue
+++ assets/src/components/shared/Pagination.vue
@@ -18,93 +18,78 @@ function pageChange(perpage) {
 
 <template>
   <div class="pagination-perpage-container">
-    <nav v-if="total_pages > 1">
-      <ul class="pagination">
-        <!-- prev nav -->
-        <li class="page-item" :class="current_page == 1 ? 'display-none' : ''">
-          <button
-            class="page-link tablenav-pages-navspan button"
-            @click="$emit('pageChange', current_page - 1)"
-            :disabled="current_page == 1"
-          >
-            Prev
-          </button>
-        </li>
-        <!-- first page -->
-        <li class="page-item" @click="$emit('pageChange', 1)" v-if="current_page != 1">
-          <button class="page-link tablenav-pages-navspan button me-1">{{ 1 }}</button>
-        </li>
-        <!-- current page - 1 -->
-        <li
-          class="page-item"
-          @click="$emit('pageChange', parseInt(current_page) - 1)"
-          v-if="current_page - 1 >= 2"
-        >
-          <button class="page-link tablenav-pages-navspan button">{{ current_page - 1 }}</button>
-        </li>
-        <!-- current page -->
-        <li class="page-item active" @click="$emit('pageChange', parseInt(current_page))">
-          <button disabled class="page-link tablenav-pages-navspan button">{{ current_page }}</button>
-        </li>
-        <!-- current page + 1 -->
-        <li
-          class="page-item"
-          @click="$emit('pageChange', parseInt(current_page) + 1)"
-          v-if="current_page + 1 <= total_pages - 1"
-        >
-          <button class="page-link tablenav-pages-navspan button">{{ current_page + 1 }}</button>
-        </li>
-        <!-- last page -->
-        <li
-          class="page-item"
-          @click="$emit('pageChange', total_pages)"
-          v-if="current_page != total_pages"
-        >
-          <button class="page-link tablenav-pages-navspan button ms-1">{{ total_pages }}</button>
-        </li>
-        <!-- next nav -->
-        <li class="page-item" :class="current_page == total_pages ? 'display-none' : ''">
-          <button
-            class="page-link tablenav-pages-navspan button"
-            @click="$emit('pageChange', parseInt(current_page) + 1)"
-            :disabled="current_page == total_pages"
-          >
-            Next
-          </button>
-        </li>
-      </ul>
-    </nav>
+    <div class="tablenav-pages" v-if="total_pages > 1">
+      <span class="pagination-links">
+        <!-- prev -->
+        <span @click="$emit('pageChange', (parseInt(current_page) - 1))"
+        class="tablenav-pages-navspan button" 
+        :class="current_page==1?'display-none':''">
+          prev
+        </span>
+
+        <!-- 1st page  -->
+        <span @click="$emit('pageChange', 1 )"
+        class="tablenav-pages-navspan button" 
+        :class="(current_page==1)||( (parseInt(current_page)-1)==1 ) ?'display-none':''" >
+          {{ 1 }}
+        </span>
+
+        <span>&nbsp; </span>
+
+        <!-- current-1  -->
+        <span @click="$emit('pageChange', (parseInt(current_page) - 1))"
+        class="tablenav-pages-navspan button" 
+        :class="current_page==1?'display-none':''" >
+          {{ (current_page - 1) }}
+        </span>
+
+        <!-- current -->
+        <span class="tablenav-pages-navspan button button-primary" >{{ current_page }}</span>
+
+         <!-- current-1  -->
+        <span @click="$emit('pageChange', (parseInt(current_page) + 1))"
+        class="tablenav-pages-navspan button"
+        :class="current_page== total_pages?'display-none':''">
+          {{ (parseInt(current_page) + 1) }}
+        </span>
+
+          <span>&nbsp; </span>
+         <!-- last page  -->
+        <span @click="$emit('pageChange', (parseInt(total_pages)))"
+        class="tablenav-pages-navspan button"
+        :class="( (current_page == total_pages) || ( (parseInt(current_page)+1)==total_pages) )  ?'display-none':''">
+          {{ (parseInt(total_pages) ) }}
+        </span>
+
+        <!-- next -->
+        <span @click="$emit('pageChange', (parseInt(current_page) + 1))"
+        :class="current_page== total_pages?'display-none':''"
+        class="tablenav-pages-navspan button " >
+          next
+        </span>
+      </span>
+    </div>
 
     <div class="ms-auto select_per_page">
-      <select
-        v-model="item_per_page"
-        @change="pageChange(item_per_page)"
-        class="form-select form-select-sm"
-        aria-label="form-select-sm example"
-      >
+      <select v-model="item_per_page" @change="pageChange(item_per_page)" class="form-select form-select-sm"
+        aria-label="form-select-sm example">
         <option value="5">5 per page</option>
         <option value="10">10 per page</option>
         <option value="20">20 per page</option>
         <option value="30">30 per page</option>
         <option value="40">40 per page</option>
         <option value="50">50 per page</option>
+        <option value="100">100 per page</option>
+        <option value="500">500 per page</option>
+        <option value="1000">1000 per page</option>
       </select>
     </div>
   </div>
 </template>
 
 <style>
-.pagination-perpage-container{
-  margin-top: 15px;
+.pagination-perpage-container{ margin:10px 0;}
+.pagination-links .tablenav-pages-navspan{
+  margin:10px 4px !important;
 }
-li.disabled.page-item {
-  cursor: not-allowed !important;
-}
-.pagination *{
-  display: inline-block;
-}
-.page-item{
-  margin-right: 4px;
-}
-
 </style>
